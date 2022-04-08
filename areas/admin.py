fd00365cb3c596d0242eb2110500e2ca8f1f4c1f
Alschn <adam.lisichin@gmail.com>
2022-04-08 14:13:49 +0200

    Add areas.json, area model and admin changes
    Added json file with every farmable area in D2 with their area levels. Made levels optional, added has_waypoint in model, add filter by lvl 85 and import export option.

--- areas/admin.py
+++ areas/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from import_export.admin import ImportExportModelAdmin
 from ordered_model.admin import OrderedModelAdmin
+
 from areas.models import Area
 
 
+class Level85AreaFilter(admin.SimpleListFilter):
+    title = "level 85"
+    parameter_name = "hell_level"
+
+    def lookups(self, request, model_admin) -> tuple[tuple[str, str]]:
+        return (
+            ('85', '85'),
+        )
+
+    def queryset(self, request, queryset) -> QuerySet[Area]:
+        if self.value() == "85":
+            return queryset.filter(hell_level=85)
+
+
 @admin.register(Area)
-class AreaAdmin(OrderedModelAdmin):
-    list_display = ('__str__', 'order', 'move_up_down_links')
+class AreaAdmin(ImportExportModelAdmin, OrderedModelAdmin):
+    list_display = (
+        '__str__',
+        'normal_level', 'nightmare_level', 'hell_level',
+        'order', 'move_up_down_links'
+    )
+    list_filter = (Level85AreaFilter, 'act')
     ordering = ('act', 'order',)
--- areas/models.py
+++ areas/models.py
@@ -20,14 +20,18 @@ class Area(OrderedModel):
     name = models.CharField(max_length=60, unique=True)
     act = models.PositiveIntegerField(choices=Act.choices)
     normal_level = models.PositiveIntegerField(
+        blank=True, null=True,
         validators=[MinValueValidator(1), MaxValueValidator(43)]
     )
     nightmare_level = models.PositiveIntegerField(
+        blank=True, null=True,
         validators=[MinValueValidator(36), MaxValueValidator(66)]
     )
     hell_level = models.PositiveIntegerField(
+        blank=True, null=True,
         validators=[MinValueValidator(67), MaxValueValidator(87)]
     )
+    has_waypoint = models.BooleanField()
 
     order_with_respect_to = "act"
 
